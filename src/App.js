7dd6a41c8693217477b877d1fa4da1869e88cc43
brightlightkim <taeyangk95@naver.com>
2022-11-02 22:33:56 -0600

    Fix the Scroll

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef} from 'react'
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 
 function App() {
   const [chatItems, setChatItems] = useState(['Welcome to EVE. How can I help you?'])
   const [message, setMessage] = useState('')
-
+  const myRef = useRef(null)
   const handleChange = (event) => {
     //Update the value of message each time it changes.
     setMessage(event.target.value)
@@ -18,20 +18,26 @@ function App() {
     fetch(`http://127.0.0.1:5000/?text=${message}`) //Fix this url with the user input
     .then(response => response.json())
     .then(json => setChatItems(oldChatItems => [...chatItems, message, json.response]))
-
     event.preventDefault() // Prevent the HTML form behavior
     setMessage('') // Reset the change
   }
 
+  const executeScroll = () => {
+    if (myRef) {
+      myRef.current?.scroll({ top: myRef.current?.scrollHeight, behavior: 'smooth' })
+    }
+  }
+
   useEffect(()=>{
     console.log(chatItems)
+    executeScroll()
   }, [chatItems]) 
 
   return (
     <>
       <Navbar/>
       <div className="container">
-        <div className='chat_container'>
+        <div className='chat_container' ref={myRef}>
           {chatItems.map((item, index)=>{
             if (index % 2 === 0){
               return <div className='nlp_chatbox' key={index}>{item}</div>
